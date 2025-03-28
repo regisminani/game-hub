22bf521996b6df44174110ca4f541c590d25f0c3
ll_git <minareg37@gmail.com>
2025-03-28 20:03:31 +0200

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,30 +1,52 @@
-import { Button, HStack, Image, List, Spinner } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  Spinner,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-    onSelectGenre: (genre: Genre) => void;
-    selectedGenre: Genre | null;
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (isLoading) return <Spinner />;
   if (error) return null;
   return (
-    <List.Root listStyle={"none"}>
-      {data.map((genre) => (
-        <List.Item key={genre.id} paddingY={"5px"}>
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              boxSize={"32px"}
-              borderRadius={8}
-            />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={()=>onSelectGenre(genre)} _hover={{textDecorationLine: 'underline'}} variant={'plain'} fontSize="lg">{genre.name}</Button>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading>Genres</Heading>
+      <List.Root listStyle={"none"}>
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize={"32px"}
+                borderRadius={8}
+                objectFit={"cover"}
+              />
+              <Button
+                textWrap={"wrap"}
+                width={"fit"}
+                textAlign={"left"}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                _hover={{ textDecorationLine: "underline" }}
+                variant={"plain"}
+                fontSize="lg"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
