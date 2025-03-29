e30faaf776751c5e091cd770a0d91a9fcbfe4577
ll_git <minareg37@gmail.com>
2025-03-29 18:56:37 +0200

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import bullsEye from '../assets/bullsEye.webp'
+import thumbsUp from '../assets/thumbsup.webp'
+import meh from '../assets/meh.webp'
+import { Image, ImageProps } from '@chakra-ui/react';
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({rating}:Props) => {
+    if(rating < 3) return null;
+    const emojiMap: {[key:number]: ImageProps} = {
+        3: {src: meh, alt: 'meh'},
+        4: {src: thumbsUp, alt: 'recommended'},
+        5: {src: bullsEye, alt: 'exceptional'},
+    }
+  return <Image {...emojiMap[rating]} boxSize='25px' marginTop={1} rounded={'l3'}/>;
+};
+
+export default Emoji;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -10,16 +11,19 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card.Root>
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <Card.Body>
-          <HStack justifyContent={"space-between"} marginBottom={3}>
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-          <Heading fontSize="2xl">{game.name}</Heading>
-        </Card.Body>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Card.Body>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
+      </Card.Body>
     </Card.Root>
   );
 };
